3651f6fe4bff3b022c7b7ed6237b8f53df2426c4
Diaz Zaid Abdurrahman <70102932+diazzaidA132EL@users.noreply.github.com>
2023-02-15 13:21:48 +0700

    Add files via upload

---
+++ firebase-project/public/scripts/index.js
@@ -0,0 +1,43 @@
+const loginElement = document.querySelector('#login-form');
+const contentElement = document.querySelector("#content-sign-in");
+const userDetailsElement = document.querySelector('#user-details');
+const authBarElement = document.querySelector("#authentication-bar");
+
+// Elements for sensor readings
+const tempElement = document.getElementById("temp");
+
+// MANAGE LOGIN/LOGOUT UI
+const setupUI = (user) => {
+  if (user) {
+    //toggle UI elements
+    loginElement.style.display = 'none';
+    contentElement.style.display = 'block';
+    authBarElement.style.display ='block';
+    userDetailsElement.style.display ='block';
+    userDetailsElement.innerHTML = user.email;
+
+    // get user UID to get data from database
+    var uid = user.uid;
+    console.log(uid);
+
+    // Database paths (with user UID)
+    var dbPathActivity = 'UsersData/' + uid.toString() + '/activity';
+
+
+    // Database references
+    var dbRefActivity = firebase.database().ref().child(dbPathActivity);
+
+    // Update page with new readings
+    dbRefActivity.on('value', snap => {
+      tempElement.innerText = snap.val().toFixed(2);
+    });
+
+  // if user is logged out
+  } else{
+    // toggle UI elements
+    loginElement.style.display = 'block';
+    authBarElement.style.display ='none';
+    userDetailsElement.style.display ='none';
+    contentElement.style.display = 'none';
+  }
+}
